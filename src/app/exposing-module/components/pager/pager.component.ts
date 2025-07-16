9d7cc5f3d49fea048b8595fe8c8acca47a0beaf9
Eugene Sokolov <greatsokol@gmail.com>
2025-07-16 13:05:05 +0300

    improve pager.component

--- src/app/exposing-module/components/pager/pager.component.ts
+++ src/app/exposing-module/components/pager/pager.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import {NgFor, NgIf, NgStyle} from "@angular/common";
 import {RouterLink} from "@angular/router";
 
@@ -8,11 +8,36 @@ import {RouterLink} from "@angular/router";
   standalone: true,
   imports: [NgStyle, NgIf, NgFor, RouterLink]
 })
-export class PagerComponent {
+export class PagerComponent implements OnInit {
   @Input("number") number: number = 0;
   @Input("size") size: number = 0;
   @Input("totalPages") totalPages: number = 0;
   @Input("totalElements") totalElements: number = 0;
   @Input("filter") filter: String | null = null;
   @Input("skipLocationChange") skipLocationChange: boolean = true;
+
+  max = 4;
+  gap = 2;
+  first: number = 0;
+  second?: number;
+  third?: number;
+
+  ngOnInit() {
+    const total = this.totalPages - 1;
+    if (this.totalPages <= this.max * 3) {
+      this.first = total;
+    } else if (this.totalPages > this.max * 3) {
+      this.first = this.max - 1;
+      this.third = total - this.max;
+      const middle = Math.floor((this.totalPages - this.first) / 2);
+      if (this.number >= this.first && this.number <= this.third) {
+        this.second = this.number + this.gap;
+      } else {
+        this.second = middle;
+      }
+    }
+  }
+
+  range = (start: number, stop: number, step: number = 1) =>
+    Array.from({length: (stop - start) / step + 1}, (_, i) => start + (i * step))
 }
